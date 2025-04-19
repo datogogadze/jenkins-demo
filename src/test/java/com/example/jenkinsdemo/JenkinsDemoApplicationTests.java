d98dfc7ba3da40d90d4038a472b0b9de1637d854
datogogadze <gogadzedato@gmail.com>
2025-04-19 14:02:22 +0400

    add context loads test

---
+++ src/test/java/com/example/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JenkinsDemoApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+}
