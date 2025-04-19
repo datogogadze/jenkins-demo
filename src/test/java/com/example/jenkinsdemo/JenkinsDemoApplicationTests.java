d422dd85b3a906e2cf7c85492975391c7dd2f02c
datogogadze <gogadzedato@gmail.com>
2025-04-19 14:36:40 +0400

    add hello test

--- src/test/java/com/example/jenkinsdemo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/example/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -1,12 +1,25 @@
 package com.example.jenkinsdemo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.jenkinsdemo.controller.HelloController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class JenkinsDemoApplicationTests {
 
+  @Autowired
+  HelloController helloController;
+
   @Test
   void contextLoads() {}
+
+  @Test
+  void test_hello() {
+    String hello = helloController.hello();
+    assertThat(hello).containsIgnoringCase("hello");
+  }
 
 }
